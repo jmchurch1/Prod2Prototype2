3668afd028fc10b8c3e8188921c1c5ce374b4843
CalDough <77810418+CalDough@users.noreply.github.com>
2022-09-13 21:40:57 -0400

    Removed start function in player movement script
    It didn't have anything in it anyways

--- Prod2Prototype2/Assets/PlayerMovement.cs
+++ Prod2Prototype2/Assets/PlayerMovement.cs
@@ -10,12 +10,6 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
